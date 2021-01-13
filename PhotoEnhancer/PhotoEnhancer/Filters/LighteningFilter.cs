90618354d6e07255771ef19ae63fa1d06ab1320b
iOnikashvili <ionikashvili@bk.ru>
2021-01-13 20:47:03 +0500

    PhotoEnhancer
    Оникашвили Илья

---
+++ PhotoEnhancer/PhotoEnhancer/Filters/LighteningFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PhotoEnhancer
+{
+    public class LighteningFilter : PixelFilter<LighteningParameters>
+    {
+        public override string ToString()
+        {
+            return "Осветление/затемнение";
+        }
+
+        public override Pixel ProcessPixel(Pixel originalPixel,
+            LighteningParameters parameters)
+        {
+            return originalPixel * parameters.Coefficient;
+        }    
+    }
+}
